97843b56440a63f591023b5cf6414879084f3f27
Kalugin68 <sashakalugin74@gmail.com>
2025-09-10 18:56:39 +0300

    extends my templates

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -7,7 +7,8 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000/
     path('about/', views.about, name='about'),
-    path('cats/<int:cat_id>/', views.categories, name='cats_id'),  # http://127.0.0.1:8000/cats/2/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/cats/fdgr45/
-    path("archive/<year4:year>/", views.archive, name='archive')# http://127.0.0.1:8000/archive/2025/
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
